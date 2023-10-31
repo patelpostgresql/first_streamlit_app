d2ec1c07c00444f2d7a934a954d42d4720809581
patelpostgresql <143235458+patelpostgresql@users.noreply.github.com>
2023-10-31 18:04:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,4 +19,4 @@ streamlit.dataframe(my_fruit_list)
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','strawberries'])
+my_fruit_list = my_fruit_list.set_index('Fruit')
