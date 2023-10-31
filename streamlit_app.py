c3273addc66b331e6e97796815ec0fb24f0aee37
patelpostgresql <143235458+patelpostgresql@users.noreply.github.com>
2023-10-31 17:03:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
-
+streamlit.title('Breakfast Manu')
 'omega 3 & Blueberry oatmeal'
+'Kale,Spinach & Rocket smoothie'
+'Hard-boiled Free Range Egg'
