dc05cc8fbc12b0c93886dc89287eba9d881e4a47
patelpostgresql <143235458+patelpostgresql@users.noreply.github.com>
2023-11-02 17:03:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
-
+streamlit.header("Fruityvice Fruit Advice!")
 
 
 
